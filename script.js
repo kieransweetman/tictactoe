0567f83ffe89d2b7f998e6e702a8bacc11839bfa
kieran Sweetman <kikisweetman@gmail.com>
2022-07-19 18:37:37 +0200

    basic game logic done

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-const Player = (name, lvl) =>{
-    this.name = name;
-    this.lvl = lvl;
+// const Player = (name, lvl) => {
+//   this.name = name;
+//   this.lvl = lvl;
 
-    const health = () => this.lvl * 3;
-    const moves = () => playerMoves(this.lvl, health()) ;
+//   const health = () => this.lvl * 3;
+//   const moves = () => playerMoves(this.lvl, health());
 
-    return {lvl, health, moves} ;
+//   return { lvl, health, moves };
+// };
 
+// const playerMoves = (lvl, health) => {
+//   let attack = () => lvl * 40;
+//   let defend = () => health() * 45;
+//   return { attack, defend };
+// };
+
+// booard module
+const Gameboard = (() => {
+  const board = () => {
+    let board = {
+      t1: null,
+      t2: null,
+      t3: null,
+      m1: null,
+      m2: null,
+      m3: null,
+      b1: null,
+      b2: null,
+      b3: null,
+    };
+    return board;
+  };
+
+  return { board };
+})();
+
+const Player = () => {
+  const turn = (move = "X") => {
+    return move == "X" ? "X" : "O";
+  };
+  return { turn };
 };
 
-const playerMoves = (lvl, health) =>{
+const Computer = (() => {
+  const turn = () => {
+    let move = Math.floor(Math.random() * 2);
+    return move;
+  };
 
-    let attack = () => lvl*40;
-    let defend = () => health()*45;
-    return {attack, defend};
+  return { turn };
+})();
 
+const p1 = Player();
+const p2 = Player();
 
+const main = ((player1, player2, board = Gameboard) => {
+  board = Gameboard.board();
 
-};
+  board.t1 = player1.turn("X");
+  board.m2 = player2.turn("O");
+  console.log(board);
+})(p1, p2);
